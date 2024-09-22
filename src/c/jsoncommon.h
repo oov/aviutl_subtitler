6793993f02f3a237a82ea7d058493c350cab8d7b
oov <ilovevipper@gmail.com>
2024-09-22 16:45:34 +0900

    clang-19

--- src/c/jsoncommon.h
+++ src/c/jsoncommon.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#ifdef __GNUC__
+#  ifndef __has_warning
+#    define __has_warning(x) 0
+#  endif
+#  pragma GCC diagnostic push
+#  if __has_warning("-Wdocumentation")
+#    pragma GCC diagnostic ignored "-Wdocumentation"
+#  endif
+#endif // __GNUC__
 #include <yyjson.h>
+#ifdef __GNUC__
+#  pragma GCC diagnostic pop
+#endif // __GNUC__
 
 struct yyjson_alc const *jsoncommon_get_json_alc(void);
